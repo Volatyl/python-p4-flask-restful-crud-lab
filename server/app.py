8e54a9d6a2364dbcd680e2f1f78bee623cfa80e5
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-23 17:20:29 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -16,6 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
 
     def get(self):
@@ -37,7 +38,9 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
+
 api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
 
@@ -45,8 +48,32 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        for key, value in request.json.items():
+            setattr(plant, key, value)
+
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        res = {
+            "deleted": True
+        }
+
+        response = make_response('', 200)
+        return response
+
+
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
